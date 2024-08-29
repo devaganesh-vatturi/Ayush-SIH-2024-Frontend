6698b3853dd41a65c085bc778af915cebf99f502
ramsai-kolli <ramsaikolli8790@gmail.com>
2024-08-29 20:50:32 +0530

    added two files

---
+++ my-react-app/src/components/StartupSingup.jsx
@@ -0,0 +1,166 @@
+import './StartupSingup.css';
+function Licensee(){
+
+    return(
+        <>
+           <div className="lmain">
+            <div className="ld1">
+                <p id="lp1">Licencensee / Applicant Registration From</p>
+            </div>
+            <div className="ld2">
+                <form className="Lfrom">
+                    <div className="ld3"> 
+                     <button id="lb1">State</button>
+                     <button id="lb2">ANDHRA PRADESH</button>
+                     </div>
+                     <div className="ld4">
+                        <lable id="ll1">1. Details of Manufacturer</lable>
+                        <lable id="ll2">(All fields Marked* are Mandatory)</lable>
+                     </div>
+                     <div className="ld5">
+                        <lable id="ll3">(i) Name of the Company/Firm*</lable>
+                        <input id="li1" type="text" placeholder="Name of the company/Firm.."></input>
+                     </div>
+                     <div className="ld6">
+                        <div className="ld7"> 
+                        <lable id="ll4">(ii) Address of Corporate Office</lable> 
+                        </div>
+                        <div className="ld8"> 
+                        <lable id="ll5">Address Line 1</lable>
+                        <input id="li2" type="text" placeholder="Address Line 1.."></input>
+                        </div>
+                        <div className="ld9">
+                        <lable id="ll6">Address Line 2</lable>
+                        <input id="li3" type="text" placeholder="Address Line 2.."></input>
+                        </div>
+                        <div className="ld10">
+                        <lable id="ll7">Address Line 3</lable>
+                        <input id="li4" type="text" placeholder="Address Line 3.."></input>
+                        </div>
+                        <div className="ld11">
+                        <lable id="ll8">Village/Town/City</lable>
+                        <input id="li5" type="text"></input>
+                        </div>
+                        <div className="ld12">
+                            <lable id="ll9">Pin Code</lable>
+                            <input id="li6" type="text"></input>
+                        </div>
+                        <div className="ld13">
+                            <lable id="ll10">State</lable>
+                             <select id="ls1">
+                                <option>-select-</option>
+                                <option value="ANDAMAN AND NICOBAR ISLAND">ANDAMAN AND NICOBAR ISLAND</option>
+                                <option value="ANDHRA PRADESH">ANDHRA PRADESH</option>
+                             </select>
+                        </div>
+                        <div className="ld14">
+                            <lable id="ll11">District</lable>
+                             <select id="ls2">
+                                <option>- please-select-</option>
+                                <option value="NICOBARS "> NICOBARS</option>
+                                <option value="we change">we change</option>
+                             </select>
+                        </div>
+                     </div>
+                     <div className="ld15">
+                        <div className="ld16">
+                            <lable id="ll12">(iii) Address of Premises</lable>
+                        </div>
+                        <div className="ld16"> 
+                        <lable id="ll13">Address Line 1</lable>
+                        <input id="li7" type="text" placeholder="Address Line 1.."></input>
+                        </div>
+                        <div className="ld17">
+                        <lable id="ll14">Address Line 2</lable>
+                        <input id="li8" type="text" placeholder="Address Line 2.."></input>
+                        </div>
+                        <div className="ld18">
+                        <lable id="ll15">Address Line 3</lable>
+                        <input id="li9" type="text" placeholder="Address Line 3.."></input>
+                        </div>
+                        <div className="ld19">
+                        <lable id="ll16">Village/Town/City</lable>
+                        <input id="li10" type="text"></input>
+                        </div>
+                        <div className="ld20">
+                            <lable id="ll17">Pin Code</lable>
+                            <input id="li11" type="text"></input>
+                        </div>
+                        <div className="ld21">
+                            <lable id="ll18">State</lable>
+                             <select id="ls3">
+                                <option>-select-</option>
+                                <option value="ANDAMAN AND NICOBAR ISLAND">ANDAMAN AND NICOBAR ISLAND</option>
+                                <option value="ANDHRA PRADESH">ANDHRA PRADESH</option>
+                             </select>
+                        </div>
+                        <div className="ld22">
+                            <lable id="ll19">District</lable>
+                             <select id="ls4">
+                                <option>- please-select-</option>
+                                <option value="NICOBARS "> NICOBARS</option>
+                                <option value="we change">we change</option>
+                             </select>
+                        </div>
+                     </div>
+                    <div className="ld23">
+                        <lable id="ll20">Note : The verification mail and all communications will be sent to the Primary Authorized Signatory</lable>
+                    </div>
+                    <div className="ld24">
+                       <lable id="ll21">(v) PAN No. of the company/ Firm</lable>
+                       <input id="li12" type="text" placeholder="PAN No. of the company/Firm"></input>
+                    </div>
+                    <div className="ld25">
+                       <lable id="ll22"> (vi) GST No. of the company/ Firm</lable>
+                       <input id="li13" type="text" placeholder="GST No. of the company/Firm"></input>
+                    </div>
+                    <div className="ld26">
+                       <lable id="ll23">  (vii) Website Address</lable>
+                       <input id="li14" type="text" placeholder="Website Address.."></input>
+                    </div>
+                    <div className="ld27">
+                        <lable id="ll24">2. Company Certification Details (If Any)</lable><br></br>
+                        <lable id="ll25">(i) Company Incorporation Certification Details</lable>
+                    </div>
+                     <div className="ld28">
+                        <lable id="ll26">(a) Certificate No.</lable>
+                        <input id="li15" type="text" ></input>
+                     </div>
+                     <div className="ld29">
+                        <lable id="ll27"> (b) Date of Issue</lable>
+                        <input id="li16" type="date" ></input>
+                     </div>
+                     <div className="ld30">
+                        <lable id="ll28"> (c) Issuing Authority </lable>
+                        <input id="li17" type="text" ></input>
+                     </div>
+                     <div className="ld31">
+                        <lable id="ll29">(ii) Details of IE Code by DGFT</lable>
+                     </div>
+                     <div className="ld32">
+                        <lable id="ll30">  (a) IE Code </lable>
+                        <input id="li18" type="text" ></input>
+                     </div>
+                     <div className="ld33">
+                        <lable id="ll31">   (b) Date of Issue</lable>
+                        <input id="li19" type="date" ></input>
+                     </div>
+                     <div className="ld34">
+                        <lable id="ll32">3. Purpose of Applying*</lable>
+                        <select id="ls5">
+                            <option>please select..</option>
+                            <option value="NEW"> NEW</option>
+                            <option value="EXISTING"> EXISTING</option>
+                        </select>
+                     </div>
+                     <div className="ld35">
+                        <input id="li20" type="text" placeholder="Enter Captcha"></input>
+                     </div>
+                     <button className="lbl">Submit</button>
+                </form>
+            </div>
+           </div>
+        </>
+    );
+}
+export default Licensee
