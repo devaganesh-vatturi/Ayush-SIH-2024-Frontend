988a5e62123b6a70c24ead3e9caa8b802cd29320
chaitanya-kadali <charankadaliaa@gmail.com>
2024-08-31 06:46:33 +0530

    none

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import Translate from './components/Translate.jsx';
 import Login from "./components/Login.jsx";
-
+import StartupSignup from './components/StartupSingup.jsx'
+import District from './components/District.jsx'; './components/District.jsx'
 function App() {
     return (
-        <div>
-          <Login/>
-          <Translate />
-        </div>
+        <>
+        <District />
+        </>
     );
 }
 
