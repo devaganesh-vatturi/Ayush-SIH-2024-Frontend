307c4b60df47d2c4883edeef40e9d9b59e3806f5
chaitanya-kadali <charankadaliaa@gmail.com>
2024-09-02 18:32:15 +0530

    completed Ai chatBot

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from 'react'
-
+import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Login from "./components/Login.jsx";
-import StartupSignup from './components/StartupSingup.jsx'   
 import Doctorsignup from './components/Doctorsignup.jsx';
 import Home from './components/Home.jsx';
-
+import StartupSignup from './components/StartupSingup.jsx';
+import Farmersignup from './components/Farmersignup.jsx';
+import AiChatBot from './components/AiChatBot.jsx'
 function App() {
     return (
-        <div>
-         {/*< Doctorsignup></Doctorsignup>*/}
-          {/*<Login/>*/}
-          <Home />
-        </div>
-
+     <>
+    {/* <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/signupstartup' element={<StartupSignup/>}/>
+    <Route path='/signupdoctor' element={<Doctorsignup/>}/>
+    <Route path='/signupfarmer' element={<Farmersignup/>}/>
+   </Routes>
+   </BrowserRouter>  */}
+   <AiChatBot />
+     </>
     );
 }
 
