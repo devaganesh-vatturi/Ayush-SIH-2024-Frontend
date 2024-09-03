8371b23c1c5e5827b836e236705d8ada5819d5f0
ramsai-kolli <ramsaikolli8790@gmail.com>
2024-09-03 18:52:55 +0530

    created PrintList.jsx and modifed in Filterdoctor.jsx,Filterfarmer.jsx

--- my-react-app/src/components/Dashboard comps/Filterfarmer.jsx
+++ my-react-app/src/components/Dashboard comps/Filterfarmer.jsx
@@ -1,7 +1,21 @@
+import PrintList from './PrintList.jsx';
 import React from 'react'
 
 export default function Filterfarmer() {
+  const faqData = [
+    {name:"raju",Email:"raj@gmail.com",phone:"9045643891",district:"west godavari"},
+    {  name:"giri",Email:"giri@gmail.com",phone:" 87345445897",district:"krishna" },
+    {  name:"venu",Email:"venu@gmail.com",phone:"9947646747",district:" kadapa" },
+    {  name:"venkat",Email:"venkat@gmail.com",phone:"9848162013",district:"east godavari" },
+  ];
+
   return (
+    <> 
     <div>Filterfarmer</div>
-  )
+    {faqData.map((obj,index) => (
+        
+        <PrintList   name1={obj.name} Email1={obj.Email} phone1={obj.phone} district1= {obj.district} ></PrintList>
+             ))}
+    </>
+  );
 }
