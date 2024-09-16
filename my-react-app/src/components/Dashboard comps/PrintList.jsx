8defdde622bf58eb5391a4ea3e4e2f5aed068f68
chaitanya-kadali <charankadaliaa@gmail.com>
2024-09-16 14:44:59 +0530

    connected backend to farmertab

--- my-react-app/src/components/Dashboard comps/PrintList.jsx
+++ my-react-app/src/components/Dashboard comps/PrintList.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import '../styles/PrintList.css';
+
  function PrintList(props){
    
-  
-   
-
+    const render=()=>{
+        if(props.usertype==="farmer"){
+            return (props.cropname.replace(/^./, str => str.toUpperCase()));
+        }else{
+            return(props.email);
+        }
+    }
   return (
         <>
              {/* <div className="d1" style={{border:"1px solid black"}}>
@@ -27,15 +32,13 @@ import '../styles/PrintList.css';
                 </div>
              </div> */}
 
-
-
 <div className="card">
     <div className="card-row">
         <div>
             <p className="name-link">{props.name1}</p>
         </div>
         <div className="text-end">
-            <p className="inline-block">{props.Email1}</p>
+            <p className="inline-block">{render()}</p>
         </div>
     </div>
     <div className="card-row">
