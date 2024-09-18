db97c5a78630200dc9aa0c720dd50fe5bf6fe96f
devaganeshvatturi <devavatturi@gmail.com>
2024-09-18 18:21:18 +0530

    deva added header styling

--- my-react-app/src/components/Startupsignup.jsx
+++ my-react-app/src/components/Startupsignup.jsx
@@ -138,7 +138,7 @@ function Startupsignup() {
     return ( 
       <>
       <Header/>
-      <form onSubmit={onSubmit}>
+      <form id="startup-form" onSubmit={onSubmit}>
         <div className="start-up-head1">
           <div className="start-up-head2">
           <p className="start-up-para">Startupsignup Details</p>
